60fa3ae4df517be02a3b7b39ae1debda1d24d809
urosjedlovcnik-commits <uros.jedlovcnik@gmail.com>
2025-10-06 19:04:28 +0200

    Add files via upload

--- admin.js
+++ admin.js
@@ -5094,11 +5094,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
                 const link = document.createElement('a');
                 const url = URL.createObjectURL(blob);
+                link.setAttribute('href', url);
                 link.setAttribute('download', 'primer_terminov_plavalcev.csv');
                 link.style.visibility = 'hidden';
                 document.body.appendChild(link);
                 link.click();
                 document.body.removeChild(link);
+                URL.revokeObjectURL(url);
             };
 
     // ===== FUNKCIJE ZA KOPIRANJE VADNIN =====
